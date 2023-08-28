4cdc2494916812a6a9a2a805d2290c7996151833
avraliov <avraliov@gmail.com>
2023-08-29 03:11:33 +0400

    Add softplus and slall changes

---
+++ src/activate-softplus.ads
@@ -0,0 +1,36 @@
+pragma Ada_2012;
+
+generic
+package Activate.Softplus is
+
+   type Softplus_Routine is tagged private;
+
+   function Get_Routine return Routine_Acc;
+   function Get_Activate (R : in Softplus_Routine) return Activate_Type;
+   function Get_Activate return Activate_Type;
+   function Get_Derivative (R : in Softplus_Routine) return Derivative_Type;
+   function Get_Activate_Enum (R : in Softplus_Routine) return Function_Enum;
+
+private
+   function Func (Item : in Matrix_Pack.Real_Vector; Idx : Positive) return Value_Type with
+     Post => Func'Result /= 0.0;
+   function DerivativeFunc (Item : in Value_Type) return Value_Type;
+
+   type Softplus_Routine is new Routine with null record;
+
+   R : aliased Softplus_Routine := Softplus_Routine'(Activate   => Func'Access,
+                                   Derivative => DerivativeFunc'Access,
+                                   Func_Enum  => Soft);
+
+   function Get_Routine return Routine_Acc is
+     (R'Access);
+   function Get_Activate (R : in Softplus_Routine) return Activate_Type is
+      (R.Activate);
+   function Get_Activate return Activate_Type is
+     (Func'Access);
+   function Get_Derivative (R : in Softplus_Routine) return Derivative_Type is
+     (R.Derivative);
+   function Get_Activate_Enum (R : in Softplus_Routine) return Function_Enum is
+     (R.Func_Enum);
+
+end Activate.Softplus;
