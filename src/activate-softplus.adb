415e16ddf90597e0f6afe1a4be80dab0dc573a04
avraliov <avraliov@gmail.com>
2023-08-29 20:33:43 +0400

    softplus DerivativeFunc modified

--- src/activate-softplus.adb
+++ src/activate-softplus.adb
@@ -1,9 +1,9 @@
 pragma Ada_2022;
 
-with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
+with Ada.Numerics.Generic_Elementary_Functions;
 
 package body Activate.Softplus is
-
+   package Elementary_Functions is new Ada.Numerics.Generic_Elementary_Functions (Float_Type => Value_Type);
    ----------
    -- Func --
    ----------
@@ -11,7 +11,7 @@ package body Activate.Softplus is
    function Func (Item : in Matrix_Pack.Real_Vector; Idx : Positive) return Value_Type is
    begin
       return Result : Value_Type do
-         Result := Value_Type (Ada.Numerics.Elementary_Functions.Log (X => (1.0 + Ada.Numerics.Elementary_Functions.Exp (Float(Item (Idx))))));
+         Result := Value_Type (Elementary_Functions.Log (X => (1.0 + Elementary_Functions.Exp (Item (Idx)))));
       end return;
    end Func;
 
@@ -21,9 +21,8 @@ package body Activate.Softplus is
 
    function DerivativeFunc (Item : in Value_Type) return Value_Type is
    begin
-      return Result : Value_Type := 0.0 do
-         Result := Value_Type ((Ada.Numerics.Elementary_Functions."**" (Ada.Numerics.E, Float (Item))) /
-                               (1.0 + (Ada.Numerics.Elementary_Functions.Exp (Float (Item)))));
+      return Result : Value_Type do
+         Result := Value_Type(1.0 / (1.0 + Elementary_Functions.Exp (X => -Item)));
       end return;
    end DerivativeFunc;
 
